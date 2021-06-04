c34994f285628171abd74cd5086b6d95601ec1d9
Fabricio Campos <fabrycio7campos7@gmail.com>
2021-06-04 17:04:34 -0600

    agregué los timestamps faltantes

--- database/migrations/2021_06_04_044531_times_spans.php
+++ database/migrations/2021_06_04_044531_times_spans.php
@@ -26,6 +26,105 @@ class TimesSpans extends Migration
         Schema::table('categoria_medicamento', function (Blueprint $table) {
             $table->timestamps();
         });
+        Schema::table('cat_diagnostico', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('centro_hospitalario', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('cita_medica', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('ciudad', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('consulta_medica', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('datos_personales', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('doctor_atiende', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('enfermeria_toma_datos', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('estado_civil', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('es_familiar_de', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('examen', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('familiar', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        
+
+
+
+        Schema::table('genero', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('hist_diagnostico', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('hist_enfermedad', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('horario', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('ingreso_hospitalario', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('medicamento', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('pais', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('parametros_citas', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('profesion', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('sala', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('signo_vital', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('sis_menu', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('sis_rol', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('sis_usuario', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('tipo_anexo', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('tipo_examen', function (Blueprint $table) {
+            $table->timestamps();
+        });
+
+        Schema::table('tipo_sangre', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('tratamiento', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        Schema::table('valor_examen', function (Blueprint $table) {
+            $table->timestamps();
+        });
+        
     }
 
 
