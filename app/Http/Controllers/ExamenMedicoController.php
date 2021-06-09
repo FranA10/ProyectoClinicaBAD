5f10d3871404b1106580cfde8f918991d0839d2e
Fabricio Campos <fabrycio7campos7@gmail.com>
2021-06-09 06:23:05 -0600

    antes de iniciar expediente

--- app/Http/Controllers/ExamenMedicoController.php
+++ app/Http/Controllers/ExamenMedicoController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Examen;
 use Illuminate\Http\Request;
 
 class ExamenMedicoController extends Controller
 {
-    public function ejemplo(){
-        return view('sistema.examen_medico.examen');
+    public function lista(){
+        $examenesPaciente=Examen::paginate(7);
+        return view('sistema.examen_medico.examenes')->with(['objetos' => $examenesPaciente] ) ;
+    }
+
+    public function crearExamen(){
+
+        return view('sistema.examen_medico.create');
     }
 }
--- routes/web/fcampos.php
+++ routes/web/fcampos.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::resource('tipo-examen', 'TipoExamenController');
 Route::resource('tipo-anexo', 'TipoAnexoController');
 
-Route::get('prueba', 'ExamenMedicoController@ejemplo');
+Route::get('examenes', 'ExamenMedicoController@lista');
+Route::get('crear-examen','ExamenMedicoController@crearExamen')->name('crear_examen');
