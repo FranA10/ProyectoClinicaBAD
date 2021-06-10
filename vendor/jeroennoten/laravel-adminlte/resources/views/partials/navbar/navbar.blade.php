1b17ee7e3a0f7c82348db5fb1b7fc5b627f33164
kevin chicas <kchiks74@gmail.com>
2021-06-10 11:31:41 -0600

    se modifica el formato de fecha

--- vendor/jeroennoten/laravel-adminlte/resources/views/partials/navbar/navbar.blade.php
+++ vendor/jeroennoten/laravel-adminlte/resources/views/partials/navbar/navbar.blade.php
@@ -18,7 +18,7 @@
 
     {{-- Navbar right links --}}
     <div style="margin-left: 35%; color:black; font-size: 20px;">
-        {{ Carbon\Carbon::now()->format('d-M-Y')}}
+        {{ Carbon\Carbon::now()->format('d M Y')}}
     </div>
     <ul class="navbar-nav ml-auto">
         {{-- Custom right links --}}
