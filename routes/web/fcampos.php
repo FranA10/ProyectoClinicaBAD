d91659a858308e6f22a3581f0297efe3775a923b
Fabricio Campos <fabrycio7campos7@gmail.com>
2021-06-11 10:50:56 -0600

    futura ruta para ver examen

--- routes/web/fcampos.php
+++ routes/web/fcampos.php
@@ -9,6 +9,9 @@ Route::resource('tipo-anexo', 'TipoAnexoController');
 Route::get('examenes', 'ExamenMedicoController@lista');
 Route::get('crear-examen','ExamenMedicoController@crearExamen')->name('crear_examen');
 
+Route::get('ver-examen','ExamenMedicoController@crearExamen')->name('crear_examen');
+
+
 Route::get('expedientes','ExpedienteController@listar');
 Route::get('expediente/{oidExpediente}','ExpedienteController@expediente');
 Route::get('expediente-crear','ExpedienteController@mostrarCrear');
